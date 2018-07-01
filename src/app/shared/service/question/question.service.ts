a339a6618e9ac9c1037882b53caf8c502ffe23b5
c5apple <c5apple_party@yahoo.co.jp>
2018-07-01 23:47:06 +0900

    キャッシュ利用

--- src/app/shared/service/question/question.service.ts
+++ src/app/shared/service/question/question.service.ts
@@ -8,13 +8,27 @@ import { ApiService } from '../api.service';
 @Injectable()
 export class QuestionService extends ApiService {
 
+  /** エンドポイント */
+  // protected url = './api/question';  // TODO API実装
+  protected url = './assets/json/question.json';
+
+  /** 問題 */
+  private questions: any[] = [];
+
   /**
    * 問題一覧を取得する
    */
   public getQuestions(): Observable<any[]> {
+    // キャッシュ利用
+    if (this.questions.length > 0) {
+      return Observable.of(this.questions);
+    }
+
     // API実行
-    const url = './api/question';
     const params = {};
-    return this.get(url, params);
+    return this.get(this.url, params).map(questions => {
+      this.questions = questions;
+      return questions;
+    });
   }
 }
